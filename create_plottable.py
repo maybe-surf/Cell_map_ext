84d8c97cb91531b4257d400fbf0de03f6f345e36
Sergei Sotnikov <97697560+maybe-surf@users.noreply.github.com>
2023-04-05 19:10:20 -0700

    Create create_plottable.py

---
+++ create_plottable.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+#detected_cells_path = "" #raw or filtered
+
+#plottable_cells_path = "" #sink where we save the file
+
+#shape = []
+
+def create_plottable_cells(detected_cells_path, plottable_cells_path, shape):
+    detected_cells = np.load(detected_cells_path)
+    plottable_cells = np.zeros((shape[0], shape[1], shape[2]))
+    
+    for line in detected_cells:
+        x = line[0]
+        y = line[1]
+        z = line[2]
+        plottable_cells[x][y][z] = 1.
+    
+    np.save(plottable_cells_path, plottable_cells)
