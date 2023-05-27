1bff08a2538742a601962964e2220463b866b488
Sergei Sotnikov <97697560+maybe-surf@users.noreply.github.com>
2023-05-26 21:22:31 -0700

    v1

--- CellMap/double_overlap.py
+++ CellMap/double_overlap.py
@@ -5,7 +5,6 @@ Created on Thu May 25 23:35:33 2023
 @author: serge
 """
 #%%
-import math
 import pandas as pd
 
 #index = 9 #???????
@@ -21,15 +20,13 @@ for cell in fos.iterrows():
         print("processing cell", cell[0])
     cell = cell[1]
     consider = mecp2[mecp2["name"] == cell["name"]]
-    for cell2 in consider.iterrows():
-        cell2 = cell2[1]
-        dist = math.sqrt((cell[0]-cell2[0])**2 + (cell[1]-cell2[1])**2 + (cell[2]-cell2[2])**2)
-        if(dist < 15):
-            if(cell2["name"] in counts.keys()):
-                counts.update({cell2["name"] : counts.get(cell2["name"])+1})
-            else:
-                counts.update({cell2["name"] : 1})
-            break
+    consider["dist_sq"] = (consider["x"]-cell[0])**2 + (consider["y"]-cell[1])**2 + (consider["z"]-cell[2])**2
+    if(consider["dist_sq"].min() < 225):
+        if(cell["name"] in counts.keys()):
+            counts.update({cell["name"] : counts.get(cell["name"])+1})
+        else:
+            counts.update({cell["name"] : 1})
+        break
     
         
     
