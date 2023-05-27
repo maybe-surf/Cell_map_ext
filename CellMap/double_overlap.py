5b1f784a616725827dc50c40e973a6070037ad8b
Sergei Sotnikov <97697560+maybe-surf@users.noreply.github.com>
2023-05-26 20:53:11 -0700

    v1

--- CellMap/double_overlap.py
+++ CellMap/double_overlap.py
@@ -6,31 +6,31 @@ Created on Thu May 25 23:35:33 2023
 """
 #%%
 import math
+import pandas as pd
 
-index = 9 #???????
-i = 0
+#index = 9 #???????
 
-mecp2 = c2
-fos = c1
+mecp2 = pd.DataFrame([])
+fos = pd.DataFrame([])
+
 
 counts = {}
 
-for cell in fos:
-    if(i % 10000 == 0):
-        print("processing cell", i)
-    i += 1
-    neighbours = {}
-    for cell2 in mecp2:
+for cell in fos.iterrows():
+    if(cell[0] % 10000 == 0):
+        print("processing cell", cell[0])
+    cell = cell[1]
+    consider = mecp2[mecp2["name"] == cell["name"]]
+    for cell2 in consider.iterrows():
+        cell2 = cell2[1]
         dist = math.sqrt((cell[0]-cell2[0])**2 + (cell[1]-cell2[1])**2 + (cell[2]-cell2[2])**2)
         if(dist < 15):
-            neighbours[dist] = cell2
-    if(len(neighbours.keys()) == 0):
-        continue
-    nearest = neighbours[min(neighbours.keys())]
-    if(nearest[index] in counts.keys()):
-        counts.update({nearest[index] : counts.get(nearest[index])+1})
-    else:
-        counts.update({nearest[index] : 1})
+            if(cell2["name"] in counts.keys()):
+                counts.update({cell2["name"] : counts.get(cell2["name"])+1})
+            else:
+                counts.update({cell2["name"] : 1})
+            break
+    
         
     
     
